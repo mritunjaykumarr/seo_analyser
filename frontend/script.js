0cf44901c2d13c190a8886c97f1ec08905ab3580
Mritunjay kumar <149924432+mritunjaykumarr@users.noreply.github.com>
2026-01-13 17:55:44 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -14,7 +14,7 @@ async function analyze() {
   }
 
   try {
-    const res = await fetch("https://seo-analyser-1.onrender.com", {
+    const res = await fetch("https://seo-analyser-1.onrender.com/analyze", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ url })
